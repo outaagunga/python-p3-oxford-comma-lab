5a30e66124719f09e174965b5a9821825956e818
outaagunga <jeconiaagunga@gmail.com>
2023-08-23 06:18:26 +0300

    Completed the lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        oxford_items = ", ".join(items[:-1])
+        return f"{oxford_items}, and {items[-1]}"
+
+# s= "geeks"
+# c= tuple(s)
+# print(c)
